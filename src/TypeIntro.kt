78857558a28336f2506c5a18aa08f387de4ceeed
Farhanunnasih <farhanunnasih@gmail.com>
2021-02-21 12:04:45 +0700

    Init

---
+++ src/TypeIntro.kt
@@ -0,0 +1,16 @@
+import java.awt.List
+
+fun main(args: Array<String>) {
+    val playerName = "Estragon"
+    val playerGold = 50
+    val experiencePoints = 5
+    var hasSteed = false;
+    val pubName = "the Unicorn's Horn"
+    val publicanName = "the Publican"
+    val pubDrinks = listOf<String>("mead", "wine", "LaCroix")
+
+
+    println(experiencePoints)
+    println(playerName.reversed())
+
+}
